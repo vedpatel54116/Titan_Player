3f95ee6a50b8643b6c4159132d64429ab3b482fc
ved patel <vedpatel54116@gmail.com>
2026-06-25 10:41:03 +0530

    test: add PlaybackEngine integration tests

---
+++ TitanPlayer/Tests/Integration/PlaybackEngineIntegrationTests.swift
@@ -0,0 +1,43 @@
+import XCTest
+@testable import TitanPlayer
+
+@MainActor
+final class PlaybackEngineIntegrationTests: XCTestCase {
+    func testFullPlaybackCycle() async throws {
+        let engine = PlaybackEngine()
+        
+        // Test initial state
+        XCTAssertEqual(engine.state, .idle)
+        
+        // Test rate setting
+        engine.setPlaybackRate(1.5)
+        XCTAssertEqual(engine.playbackRate, 1.5, accuracy: 0.001)
+        
+        // Test audio delay
+        engine.setAudioDelay(0.05)
+        XCTAssertEqual(engine.audioDelay, 0.05, accuracy: 0.001)
+        
+        // Test state transitions
+        engine.state = .ready
+        engine.play()
+        XCTAssertEqual(engine.state, .playing)
+        
+        engine.pause()
+        XCTAssertEqual(engine.state, .paused)
+        
+        engine.stop()
+        XCTAssertEqual(engine.state, .idle)
+    }
+    
+    func testGaplessCallback() async {
+        let engine = PlaybackEngine()
+        var nextURL: URL?
+        
+        engine.onNextTrack = {
+            return URL(fileURLWithPath: "/tmp/test2.mp4")
+        }
+        
+        nextURL = await engine.onNextTrack?()
+        XCTAssertNotNil(nextURL)
+    }
+}
